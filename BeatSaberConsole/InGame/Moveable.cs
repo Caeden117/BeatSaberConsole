e915125494debc53728b304e49490a824f86f3f0
Caeden Statia <caeden.s@outlook.com>
2019-01-14 22:53:47 -0800

    MONKEYMANBOY THE LEGENNNND

--- BeatSaberConsole/InGame/Moveable.cs
+++ BeatSaberConsole/InGame/Moveable.cs
@@ -8,6 +8,7 @@ using TMPro;
 using VRUIControls;
 using System.Threading;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 namespace BeatSaberConsole
 {
@@ -31,54 +32,39 @@ namespace BeatSaberConsole
 
         void Loaded(Scene arg, LoadSceneMode lolPants)
         {
-            if ((arg.name == "HealthWarning" || arg.name == "Menu") && !found) grabPointer();
+            StartCoroutine(grabPointer());
+            StartCoroutine(grabMat());
         }
 
-        Task grabPointer()
+        IEnumerator grabMat()
         {
-            return Task.Run(() => {
-                while (true)
+            yield return new WaitUntil(() => Resources.FindObjectsOfTypeAll<Material>().Where(m => m.name == "GlassHandle").Any());
+            try
+            {
+                Material material = Resources.FindObjectsOfTypeAll<Material>().Where(m => m.name == "GlassHandle").FirstOrDefault();
+                if (material) moveableMat = new Material(material);
+            }
+            catch { }
+        }
+
+        IEnumerator grabPointer() //Monkeymanboy is a fucking legend
+        {
+            yield return new WaitUntil(() => Resources.FindObjectsOfTypeAll<VRPointer>().Any());
+            try
+            {
+                VRPointer pointer = Resources.FindObjectsOfTypeAll<VRPointer>().FirstOrDefault();
+                if (pointer.gameObject.GetComponent<Movement>() == null)
                 {
-                    try
-                    {
-                        VRPointer pointer = Resources.FindObjectsOfTypeAll<VRPointer>().FirstOrDefault();
-                        if (pointer != null && found == false)
-                        {
-                            Material material = Resources.FindObjectsOfTypeAll<Material>().Where(m => m.name == "GlassHandle").FirstOrDefault();
-                            if (material) moveableMat = new Material(material);
-                            /*
-                             * Big error boi happens right here. It's preventing me from pushing out another test build.
-                             * 
-                             * Not even the ol' reliable try/catch statement can contain this beast.
-                             * IDK sometimes on scene transition/game start it crashes because of an invalid address.
-                             * Everything in output_log.txt is telling me its when adding the component, which I dont think is possible?
-                             * 
-                             * For one, a component has to have a gameobject. Two, I have an if statement up above which checks to see if
-                             * the pointer its found is not null. Movement *should* have a perfectly valid address.
-                             * 
-                             * I cant find out why.
-                             * 
-                             * If anyone can feex this and send me a PR I will forever be in debt.
-                             */
-                            if (pointer.gameObject.GetComponent<Movement>() == null)
-                            {
-                                var movePointer = pointer.gameObject.AddComponent<Movement>(); //output_log.txt leads me to believe this line of code is the source.
-                                movePointer.Init(cube.transform);
-                            }
-                            //Like literally this if statement right up here can crash the entire game.
-                            found = true;
-                            foreach (Moveable hi in FindObjectsOfType<Moveable>())
-                            {
-                                hi.found = true;
-                            }
-                            break;
-                        }
-                        else if (found) break;
-                        Thread.Sleep(10);
-                    }
-                    catch(Exception e) { Plugin.Log(e.ToString(), Plugin.LogInfo.Error); }
+                    var movePointer = pointer.gameObject.AddComponent<Movement>();
+                    movePointer.Init(cube.transform);
                 }
-            });
+                found = true;
+                foreach (Moveable hi in FindObjectsOfType<Moveable>())
+                {
+                    hi.found = true;
+                }
+            }
+            catch (Exception e) { Plugin.Log(e.ToString(), Plugin.LogInfo.Error); }
         }
 
         void Update()
